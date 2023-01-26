88f07129fe739c8872c21e8cb941a88bda3f7713
Raman Singh <mrramansingh30@gmail.com>
2023-01-26 13:39:13 -0500

    [#1] Splash screen implementation done along with icon

---
+++ app/src/main/java/com/example/pizza_singh_capstone_project/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pizza_singh_capstone_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.pizza_singh_capstone_project.utils.Constant
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(SplashScreenActivity@this,MainActivity::class.java))
+        }, Constant.setTimeOutDuration)
+    }
+}
---
+++ app/src/main/java/com/example/pizza_singh_capstone_project/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.pizza_singh_capstone_project.utils
+
+object Constant {
+    const val sharedPreferences : String = "sharedPreferences"
+    const val dateFormat : String = "EEE, MMM d, yyyy"
+    const val setTimeOutDuration : Long = 4000
+}
