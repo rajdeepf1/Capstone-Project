0d425ce3ad9451a5c013b9573c73792b76375416
rajdeepf1 <rajdeepf1@gmail.com>
2023-05-12 14:14:23 -0400

    look and feel changed for admin interface

--- app/src/main/java/com/example/pizza_singh_capstone_project/activities/OwnerHomeActivity.kt
+++ app/src/main/java/com/example/pizza_singh_capstone_project/activities/OwnerHomeActivity.kt
@@ -1,14 +1,17 @@
 package com.example.pizza_singh_capstone_project.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.pizza_singh_capstone_project.R
-import com.example.pizza_singh_capstone_project.databinding.ActivityMainBinding
 import com.example.pizza_singh_capstone_project.databinding.ActivityOwnerHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class OwnerHomeActivity : AppCompatActivity() {
@@ -21,6 +24,25 @@ class OwnerHomeActivity : AppCompatActivity() {
         binding = ActivityOwnerHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Define ActionBar object
+        // Define ActionBar object
+        val actionBar: ActionBar?
+        actionBar = supportActionBar
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        val colorDrawable = ColorDrawable(Color.parseColor("#E0AA58"))
+
+        // Set BackgroundDrawable
+
+        // Set BackgroundDrawable
+        actionBar?.setBackgroundDrawable(colorDrawable)
 
         navController = Navigation.findNavController(this, R.id.activity_owner_main_nav_host_fragment)
 
